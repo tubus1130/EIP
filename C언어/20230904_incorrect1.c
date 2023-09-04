c7419a069a0ae4a766d1377fe210d0562521de5b
김동호 <50819376+tubus1130@users.noreply.github.com>
2023-09-04 22:01:47 +0900

    Create 20230904_incorrect1.c

---
+++ C언어/20230904_incorrect1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void main(){
+    int hist[6] = {0, };
+    int n, i = 0;
+    srand(time(NULL));
+
+    do{
+        i++;
+        n = rand() % 6 + 1;
+        hist[n-1] += 1;
+    }while(i < 10);
+
+    for(int i=0; i<6; i++){
+        printf("h[%d] = %d\n", i, hist[i]);
+    }
+}
