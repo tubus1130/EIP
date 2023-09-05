eb107b427b77d32fd51c70d1a76f6cd3018a1950
김동호 <50819376+tubus1130@users.noreply.github.com>
2023-09-05 18:41:30 +0900

    Create basic4.py

---
+++ PYTHON/basic4.py
@@ -0,0 +1,32 @@
+print('Hello', end='')
+print('Hello')
+print('Hello')
+
+s = input() # 문자열형으로 받음
+s = eval(s) # 숫자로 변환
+print(s)
+print("======================")
+# swap
+a,b = 10, 20
+print(a, b)
+a,b = b,a
+print(a, b)
+print("======================")
+
+print(3 / 2)
+print(3 // 2)
+print(3 ** 2)
+print(3 % 2)
+print("======================")
+
+a, b = 3, 2
+a *= b
+print(a)
+a **= b
+print(a)
+a /= b
+print(a)
+a //= b
+print(a)
+a %= b
+print(a)
