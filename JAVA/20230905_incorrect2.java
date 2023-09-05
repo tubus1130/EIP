d67e9dcd93647c4129d008fc6f7e96b57d5358b2
김동호 <50819376+tubus1130@users.noreply.github.com>
2023-09-05 20:51:00 +0900

    Create 20230905_incorrect2.java

---
+++ JAVA/20230905_incorrect2.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// The main method must be in a class named "Main".
+class Main {
+    public static void main(String[] args) {
+        int p = 0;
+        for(int i=2; i<100; i++){
+            int t = (int)Math.sqrt(i);
+            for(int j = 2; j<=t; j++){
+                if(i%j == 0){
+                    break;
+                }
+                if(j==t){
+                    p = i;
+                }
+            }
+        }
+        System.out.println(p);
+    }
+}
