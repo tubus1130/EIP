a0ee66ab79c13f776a93a7e45b6dda4b0cf4d1a7
김동호 <50819376+tubus1130@users.noreply.github.com>
2023-09-29 21:50:02 +0900

    Create 20230929_incorrect.java

---
+++ JAVA/20230929_incorrect.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) {
+        String str = "Power overwhelming";
+        System.out.printf("%8.4s", str);
+        //     Powe
+    }
+}
