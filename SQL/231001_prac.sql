e731739bdda313fc470ba085ddff44186e8ae236
김동호 <50819376+tubus1130@users.noreply.github.com>
2023-10-01 20:32:02 +0900

    Create 231001_prac.sql

---
+++ SQL/231001_prac.sql
@@ -0,0 +1,32 @@
+CREATE INDEX idx_name ON student(name);
+
+CREATE TABLE patient(
+  id CHAR(5) PRIMARY KEY,
+  name CHAR(10),
+  sex CHAR(1),
+  phone CHAR(20),
+  CONSTRAINT sex_ck CHECK VALUE IN ('f', 'm'),
+  CONSTRAINT id_fk FOREIGN KEY(id) REFERENCES doctor(doc_id)
+);
+
+CREATE TABLE instructor(
+  id CHAR(5) PRIMARY KEY,
+  name CHAR(15) NOT NULL,
+  dept CHAR(15),
+  FOREIGN KEY(dept) REFERENCES Department(dept)
+  ON DELETE SET NULL
+  ON UPDATE CASCADE
+);
+
+ALTER TABLE patient ADD job CHAR(20);
+
+CREATE VIEW CC(ccid, ccname, instname) AS
+(
+  SELECT A.id, A.name, B.name
+  FROM Course A, Instructor B
+  WHERE Course.id = Instructor.id
+)
+
+CREATE UNIQUE INDEX Stud_idx ON Student(ssn ASC);
+
+CREATE INDEX 직원_name ON 직원(이름);
