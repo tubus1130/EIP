fa3a4a225fa79550266907893a5cb67852c530b4
김동호 <50819376+tubus1130@users.noreply.github.com>
2023-10-01 20:23:47 +0900

    Create memo1.sql

---
+++ SQL/memo1.sql
@@ -0,0 +1,37 @@
+-- 스키마 생성
+CREATE SCHEMA 스키마명 AUTHORIZATION 사용자_ID;
+
+-- 도메인 생성
+CREATE DOMAIN 도메인명 데이터타입
+[DEFAULT 기본값]
+[CONSTRAINT 제약조건명 CHECK (범위값)];
+
+-- 테이블 생성
+CREATE TABLE 테이블명(
+  속성명 데이터타입 [DEFAULT 기본값] [NOT NULL],
+  [PRIMARY KEY(기본키)],
+  [FOREIGN KEY(외래키) REFERENCES 참조테이블(기본키)
+  ON DELETE 옵션
+  ON UPDATE 옵션],
+  [CONSTRAINT 제약조건명 CHECK(범위값)] 
+);
+
+-- 뷰 생성
+CREATE VIEW 뷰명(속성명, 속성명, ...) AS 
+  SELECT문;
+
+-- 인덱스 생성
+CREATE INDEX 인덱스명 ON 테이블명(속성명);
+
+-- 테이블 변경
+ALTER TABLE 테이블명 ADD 속성명 데이터타입;
+ALTER TABLE 테이블명 ALTER 속성명 데이터타입;
+ALTER TABLE 테이블명 DROP COLUMN 속성명;
+
+-- DROP
+DROP SCHEMA 스키마명;
+DROP DOMAIN 도메인명;
+DROP TABLE 테이블명;
+DROP VIEW 뷰명;
+DROP INDEX 인덱스명;
+DROP CONSTRAINT 제약조건명;
